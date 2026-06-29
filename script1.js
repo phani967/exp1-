3a58275296b772b999ac7eecb3e75066d24ca3d7
phani967 <reddypphani@gmail.com>
2026-06-29 15:10:57 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,2 @@
+console.log("Hello from javascript");
+alert("Java script is successfull linked");
---
+++ script5.js
@@ -0,0 +1,16 @@
+function greet() 
+{ 
+} 
+console.log("Hello from a function declaration!"); 
+const sayGoodbye = function() 
+{ 
+console.log("Goodbye from a function expression!"); 
+}; 
+const sayThanks = () => 
+{ 
+console.log("Thanks from an arrow function!"); 
+}; 
+greet(); 
+function sayGoodbye(); 
+function expression sayThanks(); 
+Calls the arrow function
